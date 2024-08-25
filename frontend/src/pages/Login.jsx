6c1edfe5f829452bba20c85ebcfe54c668b3b74f
Syed Asfar Ahmad Bukhari <85727853+syed-asfar-ahmad@users.noreply.github.com>
2024-08-25 17:54:12 +0500

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const response = await axios.post(
-          "http://localhost:4000/api/v1/user/login",
+          "https://pel-medical-care.onrender.com/api/v1/user/login",
           { email, password, confirmPassword, role: "Patient" },
           {
             withCredentials: true,
