8a0bd0e039c2363b433e629fe788fd6cfb203972
Syed Asfar Ahmad Bukhari <85727853+syed-asfar-ahmad@users.noreply.github.com>
2024-08-25 17:51:25 +0500

    Update AppointmentForm.jsx

--- frontend/src/components/AppointmentForm.jsx
+++ frontend/src/components/AppointmentForm.jsx
@@ -34,7 +34,7 @@ const AppointmentForm = () => {
   useEffect(() => {
     const fetchDoctors = async () => {
       const { data } = await axios.get(
-        "http://localhost:4000/api/v1/user/doctors",
+        "https://pel-medical-care.onrender.com/api/v1/user/doctors",
         { withCredentials: true }
       );
       setDoctors(data.doctors);
