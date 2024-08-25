0c5b6ca8117b9646b436affe5dca3f978262e650
Syed Asfar Ahmad Bukhari <85727853+syed-asfar-ahmad@users.noreply.github.com>
2024-08-25 17:05:51 +0500

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,5 @@
 import app from "./app.js";
-import cloudinary from "cloudinary"
-const port = process.env.PORT || 4000;
+import cloudinary from "cloudinary";
 
 cloudinary.v2.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
