12d858994008dd38b01735c2f4b9e35e1b16af93
Syed Asfar Ahmad Bukhari <85727853+syed-asfar-ahmad@users.noreply.github.com>
2024-08-25 17:53:20 +0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
   useEffect(()=>{
     const fetchUser = async()=>{
       try {
-        const response = await axios.get("http://localhost:4000/api/v1/user/patient/me", {withCredentials: true});
+        const response = await axios.get("https://pel-medical-care.onrender.com/api/v1/user/patient/me", {withCredentials: true});
         setIsAuthenticated(true);
         setUser(response.data.user);
       } catch (error) {
