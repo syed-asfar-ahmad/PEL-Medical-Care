82c56816b58f214cc89f9901fe7f43887f882613
mtalhashafiq123 <m.talha.shafiq@ezeelogix.com>
2024-08-29 23:41:46 +0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect } from "react";
-import "./App.css"
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Appointment from "./pages/Appointment";
 import AboutUs from "./pages/AboutUs";
@@ -13,40 +13,51 @@ import Footer from "./components/Footer";
 import { Context } from "./main";
 import axios from "axios";
 
-
 const App = () => {
-  const { setIsAuthenticated, setUser} = useContext(Context);
-  useEffect(()=>{
-    const fetchUser = async()=>{
-      try {
-        const response = await axios.get("https://pel-medical-care.onrender.com/api/v1/user/patient/me", {withCredentials: true});
-        setIsAuthenticated(true);
-        setUser(response.data.user);
-      } catch (error) {
-        setIsAuthenticated(false);
-        setUser({});
-      }
-    };
-    fetchUser();
-  }, [setIsAuthenticated, setUser]
-);
+  const { setIsAuthenticated, setUser } = useContext(Context);
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem('user');
+    const storedAuth = localStorage.getItem('isAuthenticated');
+
+    if (storedUser && storedAuth) {
+      setIsAuthenticated(JSON.parse(storedAuth));
+      setUser(JSON.parse(storedUser));
+    } else {
+      const fetchUser = async () => {
+        try {
+          const response = await axios.get("https://pel-medical-care.onrender.com/api/v1/user/patient/me", { withCredentials: true });
+          setIsAuthenticated(true);
+          setUser(response.data.user);
+          localStorage.setItem('user', JSON.stringify(response.data.user));
+          localStorage.setItem('isAuthenticated', 'true');
+        } catch (error) {
+          setIsAuthenticated(false);
+          setUser({});
+          localStorage.removeItem('user');
+          localStorage.removeItem('isAuthenticated');
+        }
+      };
+      fetchUser();
+    }
+  }, [setIsAuthenticated, setUser]);
+
   return (
     <>
-    <Router>
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/appointment" element={<Appointment/>}/>
-        <Route path="/about" element={<AboutUs/>}/>
-        <Route path="/register" element={<Register/>}/>
-        <Route path="/login" element={<Login/>}/>
-      </Routes>
-      <Footer/>
-      <ToastContainer position="top-center"/>
-    </Router>
-
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/appointment" element={<Appointment />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+        <ToastContainer position="top-center" />
+      </Router>
     </>
-  )
-}
+  );
+};
 
 export default App;
