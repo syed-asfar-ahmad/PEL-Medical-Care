93031f337b968936cf94cc4c4086202872018cd6
mtalhashafiq123 <m.talha.shafiq@ezeelogix.com>
2024-08-29 22:58:56 +0500

    Update jwtToken.js

--- Backend/utils/jwtToken.js
+++ Backend/utils/jwtToken.js
@@ -2,8 +2,10 @@ export const generateToken = (user, message, statusCode, res)=>{
     const token = user.generateJsonWebToken();
     const cookieName = user.role === "Admin" ? "adminToken" : "patientToken";
     res.status(statusCode).cookie(cookieName, token, {
-        expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 *60 *1000),
+        expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
         httpOnly: true,
+         secure: process.env.NODE_ENV === 'production', 
+         sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
     }).json({
         success: true,
         message,
